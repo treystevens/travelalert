97bba22ab836d123469964407c8eb4c2ba5ba0df
treykris <trey10027@gmail.com>
2019-01-21 19:58:19 -0500

    slight clean up of comments and added semicolons

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import * as inquirer from 'inquirer';
 import { crawlersArray, loader } from './loader';
-const airports = require('../airports.json')
-
+const airports = require('../airports.json');
 
 inquirer
   .prompt([
@@ -10,7 +9,7 @@ inquirer
         name: 'origin',
         message: `Where are you flying from?`,
         filter: ((origin: string) => {
-            return origin.toUpperCase()
+            return origin.toUpperCase();
         })
     },
     {
@@ -18,27 +17,22 @@ inquirer
         name: 'dest',
         message: `Where are you flying to?`,
         filter: ((dest: string) => {
-            return dest.toUpperCase()
+            return dest.toUpperCase();
         })
     },
   ])
-  .then(answers => {
+  .then((answers: {origin: string, dest: string}) => {
 
     const originCity = airports[answers.origin].city;
     const destCity = airports[answers.dest].city;
 
-    // const testSoleClass = new GoogleNews(originCity, destCity)
-    // testSoleClass.getData()
-
-
-    loader(originCity, destCity)
+    loader(originCity, destCity);
     crawlersArray.forEach((crawler) => {
         crawler.getData();
     })
-      
   })
   .catch((err: Error) => {
-      console.log(err)
+      console.log(err);
   })
 
 
