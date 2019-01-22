3df512a82f1a4bbc0ecc0577f5ddb49534cbb9f6
treykris <trey10027@gmail.com>
2019-01-21 19:53:23 -0500

    Added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.ts',
+  devtool: 'cheap-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  target: 'node',
+  mode: 'development'
+};
