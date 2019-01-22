7e12c0d5c171a61e795dbfa1ac8ca753be77eda3
treykris <trey10027@gmail.com>
2019-01-21 19:58:54 -0500

    Refactored variables to instantiate new Crawler class

--- src/loader.ts
+++ src/loader.ts
@@ -1,15 +1,13 @@
-import { Crawler } from './interface';
+import Crawler from './Crawler';
 import Reuters from './crawlers/reuters';
 import GoogleNews from './crawlers/googlenews';
-
 
 export const crawlersArray: Array<Crawler> = [];
 
 export function loader(origin: string, dest: string):void{
 
-    const reuters = new Reuters(origin, dest);
-    const googleNews = new GoogleNews(origin, dest);
+    const reuters: Crawler = new Reuters(origin, dest);
+    const googleNews: Crawler = new GoogleNews(origin, dest);
 
-    crawlersArray.push(reuters, googleNews)
-
+    crawlersArray.push(reuters, googleNews);
 }
